fe33d971a2aac48d4267d80a397bde48fb9032fc
iqbal <iqbal.bcss19@iba-suk.edu.pk>
2022-09-20 01:26:28 +0500

    back-end logic

--- app/src/main/java/com/example/guessnumber/MainActivity.kt
+++ app/src/main/java/com/example/guessnumber/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.guessnumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var random:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        random = Random().nextInt(1000)
+    }
+    fun validate(view: View){
+        val lbl: TextView = findViewById(R.id.lblResults)
+        val num: EditText = findViewById(R.id.inputNum)
+
+        val number = Integer.parseInt(num.text.toString())
+        if(number > random){
+            lbl.text = "Too High"
+        }
+        else if (number < random)
+            lbl.text = "too low"
+        else
+            lbl.text = "you guessed correctly.."
     }
 }
